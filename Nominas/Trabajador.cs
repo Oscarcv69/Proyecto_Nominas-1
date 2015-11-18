3df237adb9d8fff1dc03315a1f10a11c51cb7b70
Miguel Zumaquero Bernal <zumaquero8@gmail.com>
2015-11-18 11:23:33 +0100

    Creacion de  dni_pre, nombre_pre y apellidos_pre

--- Nominas/Trabajador.cs
+++ Nominas/Trabajador.cs
@@ -8,7 +8,44 @@ namespace Nominas
 {
     class Trabajador
     {
-        
+        private string dni, nombre, apellidos;
+        public Trabajador() {
+            this.dni = null;
+            this.nombre = null;
+            this.apellidos = null;
+        }
+        public string dni_pre
+        {
+            get
+            {
+                return dni;
+            }
+            set
+            {
+                
+            }
+        }
+        public string nombre_pre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
 
+            }
+        }
+        public string apellidos_pre
+        {
+            get
+            {
+                return apellidos;
+            }
+            set
+            {
+
+            }
+        }
     }
 }
