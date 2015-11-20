dd2045abde070177ea892934320d8f023529f01d
Miguel Zumaquero Bernal <zumaquero8@gmail.com>
2015-11-20 09:25:03 +0100

    los validadores de los set

--- Nominas/Trabajador.cs
+++ Nominas/Trabajador.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Nominas
@@ -22,7 +23,19 @@ namespace Nominas
             }
             set
             {
-                
+                int  numero = 0;
+                if (value.Length < 9)
+                {
+                    throw new Exception("La longitud debe ser 9");
+                }
+                else if (!Int32.TryParse(value.Substring(0, 8), out numero) || (Int32.TryParse(value[8].ToString(), out numero)))
+                {
+                    throw new Exception("DNI Incorrecto: Formato erróneo (22334455A)");
+                }
+                else
+                {
+                    dni = value.ToUpper();
+                }
             }
         }
         public string nombre_pre
@@ -33,7 +46,14 @@ namespace Nominas
             }
             set
             {
-
+                if (Regex.IsMatch(value.ToString(), @"^[a-zA-Z ]+$"))
+                {
+                    nombre = value;
+                }
+                else
+                {
+                    throw new Exception("El nombre introducido no es correcto");
+                }
             }
         }
         public string apellidos_pre
@@ -44,7 +64,14 @@ namespace Nominas
             }
             set
             {
-
+                if (Regex.IsMatch(value.ToString(), @"^[a-zA-Z ]+$"))
+                {
+                    apellidos = value;
+                }
+                else
+                {
+                    throw new Exception("El apellido introducido no es correcto");
+                }
             }
         }
     }
