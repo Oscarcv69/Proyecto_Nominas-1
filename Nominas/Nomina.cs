f2ab56a5f6f28ac9d1dc0aedf2b1b44a9377c17d
Nachh <nachomm11@gmail.com>
2015-11-28 05:42:25 +0100

    Avanzando un poco
    Variables, Constructor, Propiedades y Control de errores.

--- Nominas/Nomina.cs
+++ Nominas/Nomina.cs
@@ -2,28 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Nominas
-{
-    class Nomina{
-		private int horas;
+namespace Nominas {
+    class Nomina {
 
-		public Nomina(){
-			
-			this.horas = null;
-		}
-	
-		//Fragmento estándar
-		public int Horas{
-			
-			set{
-				this.horas = value;
-			}
+        private short horas;
+        private short horasE; //Horas Extra
+        private float impuestos;
 
-			get{
-				return horas;
-			}
-		}
+        private float salario;
+        private float salarioE; // Extra
+        private float salarioB; // Bruto
+        private float salarioN; // Neto
+
+        Regex regex = new Regex("`[0-9]*$"); // regex only numbers
+
+        public Nomina() {
+
+            this.horas = 0;
+            this.horasE = 0;
+            this.impuestos = 0;
+            this.salario = 0;
+            this.salarioE = 0;
+            this.salarioB = 0;
+            this.salarioN = 0;
+        }
+
+        #region PROPIEDADES
+
+        public short Horas {
+
+            set {
+                this.horas = value;
+            }
+
+            get {
+                if(!regex.IsMatch(horas.ToString())) {
+                    Console.WriteLine("Debes introducir valores numéricos");
+                }
+
+                return horas;
+            }
+        }
+
+        public short HorasE {
+            set {
+                this.horasE = value;
+            }
+
+            get {
+                if(!regex.IsMatch(horasE.ToString())) {
+                    Console.WriteLine("Debes introducir valores numéricos");
+                }
+
+                return horasE;
+            }
+        }
     }
+
+
+    #endregion
 }
+
