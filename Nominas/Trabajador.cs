9af92a421bb19c2ed1d7e2a54eef937a8c011bae
Miguel Zumaquero Bernal <zumaquero8@gmail.com>
2015-11-29 20:30:54 +0100

    dni letra

--- Nominas/Trabajador.cs
+++ Nominas/Trabajador.cs
@@ -43,7 +43,7 @@ namespace Nominas
                     {
                         throw new Exception("DNI Incorrecto: Letra del DNI es incorrecta");
                     }
-                    else
+                    else if (letra == comprobacion[resto])
                     {
                         dni = value.ToUpper();
                     }
